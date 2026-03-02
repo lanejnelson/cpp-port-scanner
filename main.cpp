8d5c098df1795ef8daedaac04d9e3598221a90e1
lane <lanene@umich.edu>
2026-03-01 20:53:07 -0500

    Added summaries and basic service mapping with a list of known services

--- main.cpp
+++ main.cpp
@@ -8,6 +8,27 @@
 
 using namespace std;
 
+static unordered_map<int, string> port_map {{21, "FTP"}, {22, "SSH"}, {25, "SMTP"}, {80, "HTTP"}, {443, "HTTPS"}};
+
+void print_summary(vector<int> open_ports, int total_ports) {
+    int open_count = open_ports.size();
+    int closed_count = total_ports - open_count;
+    //cout << endl << endl;
+    cout << "-----------------------------------" << endl;
+    for (auto port: open_ports) {
+        if (port_map.count(port) != 0) {
+            cout << port << ": " << port_map[port] << endl;
+        }
+        else {
+            cout << port << ": " << "Unknown Service" << endl;
+        }
+    }
+    cout << "-----------------------------------" << endl;
+    cout << "TOTAL" << endl;
+    cout << open_count << " OPEN ports" << endl;
+    cout << closed_count << " CLOSED ports" << endl;
+}
+
 int main(int argc, char* argv[]) {
     if (argc != 3) {
         cout << "USAGE: ./scan <IP> <HIGHEST PORT>" << endl;
@@ -23,6 +44,7 @@ int main(int argc, char* argv[]) {
     const char * ip_c_str = ip.c_str();
     address.sin_addr.s_addr = inet_addr(ip_c_str);
     int highest_port = stoi(argv[2]);
+    vector<int> open_ports;
     // Create socket for connections
 
     // Loop through all ports
@@ -32,12 +54,14 @@ int main(int argc, char* argv[]) {
         int success = connect(sockfd, (struct sockaddr*)&address, sizeof(address));
         if (success == 0) {
             cout << "Port " << port << " is open!" << endl;
+            open_ports.push_back(port);
         }
         else {
             cout << "Port " << port << " is closed!" << endl;
         }
         close(sockfd);
     }
+    print_summary(open_ports, highest_port);
     //cout << "Hello world!" << endl;
     return 0;
 }
